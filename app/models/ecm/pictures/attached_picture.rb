9ce7cd248004fdde0daf781a4c04f84bcdd94238
Roberto Vasquez Angel <roberto@vasquez-angel.de>
2016-06-27 00:08:08 +0200

    Removed attribute_accessible support.

--- app/models/ecm/pictures/attached_picture.rb
+++ app/models/ecm/pictures/attached_picture.rb
@@ -4,9 +4,6 @@ module Ecm::Pictures
     belongs_to :picture, foreign_key: 'ecm_pictures_picture_id'
     belongs_to :pictureable, polymorphic: true
 
-    # attributes
-    attr_accessible :ecm_pictures_picture_id if respond_to?(:attr_accessible)
-
     # validations
     validates :picture, :pictureable, presence: true
   end
