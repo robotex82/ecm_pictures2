0d0ef82b902f20f4728538e9852237dbe9f3ef95
Roberto Vasquez Angel <roberto@vasquez-angel.de>
2017-05-07 17:21:26 +0200

    Localize routes by default.

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Ecm::Pictures::Engine.routes.draw do
   localized do
-    resources :galleries, only: [:index, :show]
-    resources :pictures,  only: [:index, :show]
+    scope :ecm_pictures do
+      resources :galleries, only: [:index, :show]
+      resources :pictures,  only: [:index, :show]
+    end
+    root to: 'galleries#index'
   end
 end
